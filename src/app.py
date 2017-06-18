c505b6bd4d2a368e1bcd44bcdff8e4b48b4044c4
Tim Gross <tim@0x74696d.com>
2017-06-18 14:44:27 -0400

    Added connection and libraries required for MySQL
    The application as originally authored is not suited to be deployed for
    horizontal scalability because of its reliance on a sqlite database on
    the local file system. This allows us to replace sqlite with an instance
    of MySQL.

--- src/app.py
+++ src/app.py
@@ -1,7 +1,14 @@
+import os
+import sqlite3
+
 from flask import Flask, request, redirect, url_for
-import sqlite3 as sql
+import pymysql
+
 application = Flask(__name__)
 
+application.config['db_host'] = os.environ.get('DB_HOST', '')
+application.config['db_user'] = os.environ.get('DB_USER', 'user')
+application.config['db_passwd'] = os.environ.get('DB_PASSWD', 'passwd')
 
 @application.route('/')
 def hello_world():
@@ -22,7 +29,19 @@ def fail():
 def create():
     try:
         username = request.form['username']
-        con = sql.connect("database.db")
+
+        # realistically this is a terrible idea because we're likely
+        # to have DB-specific queries that won't be compatible between
+        # sqlite and MySQL
+        if not application.config.get('db_host'):
+            con = sql.connect('database.db')
+        else:
+            con = pymysql.connect(host=application.config['db_host'],
+                                  user=application.config['db_user'],
+                                  password=application.config['db_passwd'],
+                                  db='db',
+                                  charset='utf8mb4',
+                                  cursorclass=pymysql.cursors.DictCursor)
         cur = con.cursor()
         cur.execute("INSERT INTO users (username) VALUES (?)", (username, ))
         con.commit()
